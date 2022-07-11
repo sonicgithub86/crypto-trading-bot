cce8cd322486dfc0c5defb9791b2e2db99070aed
sonicgithub86 <sonic.github@gmail.com>
2022-07-11 22:17:57 +0300

    Add strategy to 30mins

--- src/modules/strategy/strategies/stocha_strat.js
+++ src/modules/strategy/strategies/stocha_strat.js
@@ -3,23 +3,56 @@ const TA = require('../../../utils/technical_analysis');
 
 const SignalResult = require('../dict/signal_result');
 
+let flags = {
+  emaCrossUp: false,
+  emaCrossDown: false,
+  stoch30Up: undefined,
+  stoch30Down: undefined,
+  stoch30CrossedUp: undefined,
+  stoch30CrossedDown: undefined,
+  stoch5Up: undefined,
+  stoch5Down: undefined,
+  stoch5CrossedUp: undefined,
+  stoch5CrossedDown: undefined,
+  stoch1Up: undefined,
+  stoch1Down: undefined,
+  stoch1CrossedUp: undefined,
+  stoch1CrossedDown: undefined
+};
+
+let resetAllFlags = function resetAllFlags() {
+  flags = {
+    emaCrossUp: false,
+    emaCrossDown: false,
+    stoch30Up: undefined,
+    stoch30Down: undefined,
+    stoch30CrossedUp: undefined,
+    stoch30CrossedDown: undefined,
+    stoch5Up: undefined,
+    stoch5Down: undefined,
+    stoch5CrossedUp: undefined,
+    stoch5CrossedDown: undefined,
+    stoch1Up: undefined,
+    stoch1Down: undefined,
+    stoch1CrossedUp: undefined,
+    stoch1CrossedDown: undefined
+  };
+}
+
+//   stoch1Down;
 module.exports = class {
   getName() {
     return 'stocha_strat';
   }
 
   buildIndicator(indicatorBuilder, options) {
-    indicatorBuilder.add('ema', 'ema', '30m', {
+    indicatorBuilder.add('ema12', 'ema', '30m', {
       length: 12
     });
-    indicatorBuilder.add('ema', 'ema', '30m', {
+    indicatorBuilder.add('ema34', 'ema', '30m', {
       length: 34
     });
-    // indicatorBuilder.add('stoch', 'stoch', '30m', {
-    //   length: 5,
-    //   k: 3,
-    //   d: 3
-    // });
+
     indicatorBuilder.add('stoch30', 'stoch', '30m', {
       length: 5,
       k: 3,
@@ -35,105 +68,82 @@ module.exports = class {
       k: 3,
       d: 3
     });
-
-
-    /*indicatorBuilder.add('bb', 'bb', '15m');
-    indicatorBuilder.add('rsi', 'rsi', '15m');
-    indicatorBuilder.add('mfi', 'mfi', '15m');
-    indicatorBuilder.add('volume_profile', 'volume_profile', '15m');
-    indicatorBuilder.add('zigzag', 'zigzag', '15m');
-
-    indicatorBuilder.add('pivot_points_high_low', 'pivot_points_high_low', '15m', {
-      left: 14,
-      right: 14
-    });
-
-    indicatorBuilder.add('sma200', 'sma', '15m', {
-      length: 200
-    });
-
-    indicatorBuilder.add('sma50', 'sma', '15m', {
-      length: 50
-    });
-
-    indicatorBuilder.add('foreign_candle', 'candles', options.foreign_pair_period || '15m', {
-      exchange: options.foreign_pair_exchange || 'binance',
-      symbol: options.foreign_pair_symbol || 'BTCUSDT'
-    });*/
   }
 
   async period(indicatorPeriod, options) {
     const currentValues = indicatorPeriod.getLatestIndicators();
-
-    const bollinger = indicatorPeriod.getIndicator('bb');
-    const stoch1 = indicatorPeriod.getIndicator('stoch1');
-    console.log('asdasdasdasd=====================');
     if (currentValues) {
-      console.log(Object.keys(currentValues));
+      // console.log(Object.keys(currentValues));
     }
 
-    if (stoch1) {
-      console.log('Stoch1 Values');
-      var stochKeys = Object.keys(stoch1);
-      console.log('Stoch1 Values LENGTH ' + stochKeys.length);
-      // stochKeys.forEach(function (key) {
-      //   var stoch = stoch1[key];
-      //   console.log('Stoch_d ' + stoch.stoch_d);
-      //   console.log('Stoch_k ' + stoch.stoch_k);
-      //   console.log('\n');
-      // });
-    }
-    if (bollinger && currentValues.bb) {
-      const standardDeviation = SD.calculate({
-        period: 150,
-        values: bollinger.slice(-200).map(b => b.width)
-      });
+    const ema12 = currentValues.ema12;
+    const ema34 = currentValues.ema34;
 
-      currentValues.bb.sd = standardDeviation.slice(-1)[0];
-    }
-
-    const currentBB = indicatorPeriod.getLatestIndicator('bb');
-    if (currentBB && currentValues.bb) {
-      currentValues.bb.percent = TA.getBollingerBandPercent(
-        indicatorPeriod.getPrice(),
-        currentBB.upper,
-        currentBB.lower
-      );
-    }
-
-    const intl = new Intl.NumberFormat('en-US', { minimumSignificantDigits: 3, maximumSignificantDigits: 4 });
-
-    currentValues.ranges = (indicatorPeriod.getIndicator('volume_profile') || [])
-      .sort((a, b) => b.totalVolume - a.totalVolume)
-      .slice(0, 3)
-      .map(v => `${intl.format(v.rangeStart)}-${intl.format(v.rangeEnd)}`)
-      .join(', ');
+    const stoch30 = currentValues.stoch30;
+    const stoch5 = currentValues.stoch5;
+    const stoch1 = currentValues.stoch1;
 
     const emptySignal = SignalResult.createEmptySignal(currentValues);
 
-    // entry or exit
     if (!indicatorPeriod.getLastSignal()) {
-      const dice = parseFloat(options.dice || 6);
-      const diceSize = parseFloat(options.dice_size || 12);
+      if (ema12 && ema34 && stoch30) {
+        // debugger;
+        const currentEma30Up = ema12 > ema34;
+        const ema30Down = ema12 < ema34;
 
-      const number = Math.floor(Math.random() * diceSize) + 1;
-      emptySignal.addDebug('message', `${number}`);
-      if (number === dice) {
-        const longOrShort = Math.random() > 0.5 ? 'long' : 'short';
-        emptySignal.setSignal(longOrShort);
+        if (flags.emaCrossUp !== currentEma30Up) {
+          resetAllFlags();
+        }
+
+        const stoch30Up = stoch30.stoch_k > stoch30.stoch_d;
+        const stoch30Down = stoch30.stoch_k < stoch30.stoch_d;
+
+        flags.stoch30CrossedUp =
+          flags.stoch30CrossedUp ||
+          (typeof flags.stoch30Up !== 'undefined' &&
+            flags.stoch30Up !== stoch30Up &&
+            stoch30Up
+          );
+
+        flags.stoch30CrossedDown =
+          flags.stoch30CrossedDown ||
+          (typeof flags.stoch30Down !== 'undefined' &&
+          flags.stoch30Down !== stoch30Down &&
+          stoch30Down);
+
+        if (currentEma30Up && flags.stoch30CrossedUp) {
+          console.log('stoch30CrossedUp');
+          emptySignal.setSignal('long');
+          console.log(`Price LONG: ${indicatorPeriod.getPrice()}`);
+        } else if (ema30Down && flags.stoch30CrossedDown) {
+          console.log('stoch30CrossedDown');
+          emptySignal.setSignal('short');
+          console.log(`Price SHORT: ${indicatorPeriod.getPrice()}`);
+        }
+
+        flags.emaCrossUp = currentEma30Up;
+        flags.emaCrossDown = ema30Down;
+        flags.stoch30Up = stoch30Up;
+        flags.stoch30Down = stoch30Down;
       }
     }
 
     // close on profit or lose
     if (indicatorPeriod.getLastSignal()) {
-      if (indicatorPeriod.getProfit() > 2) {
+      // console.log(`Price: ${indicatorPeriod.getPrice()}`);
+      // console.log(`Amount: ${indicatorPeriod.getStrategyContext().amount}`);
+      if (indicatorPeriod.getProfit() > 1.7) {
         // take profit
+        console.log(`Price TAKE PROFIT: ${indicatorPeriod.getPrice()}`);
         emptySignal.addDebug('message', 'TP');
         emptySignal.setSignal('close');
-      } else if (indicatorPeriod.getProfit() < -2) {
+        resetAllFlags();
+      } else if (indicatorPeriod.getProfit() < -0.15) {
         // stop loss
+        console.log(`Price STOP LOSS: ${indicatorPeriod.getPrice()}`);
         emptySignal.addDebug('message', 'SL');
         emptySignal.setSignal('close');
+        resetAllFlags();
       }
     }
 
@@ -196,12 +206,11 @@ module.exports = class {
 
   getOptions() {
     return {
-      period: '15m',
+      period: '1m',
       dice: 6,
       dice_size: 12,
       foreign_pair_exchange: 'binance',
-      foreign_pair_symbol: 'BTCUSDT',
-      foreign_pair_period: '15m'
+      foreign_pair_period: '1m'
     };
   }
 
